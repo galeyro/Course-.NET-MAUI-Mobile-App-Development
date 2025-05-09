7c88c3d580ea9497d445649dd8eb82b5e3770531
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-05-09 09:14:16 -0500

    Defining Platform Specific Values

--- MyFirstMauiApp/LayourExample.xaml.cs
+++ MyFirstMauiApp/LayourExample.xaml.cs
@@ -5,5 +5,8 @@ public partial class LayourExample : ContentPage
 	public LayourExample()
 	{
 		InitializeComponent();
+
+		VStackLayout.Padding = DeviceInfo.Platform == DevicePlatform.iOS
+			? new Thickness(30,10,30,10) : new Thickness(25);
 	}
 }
